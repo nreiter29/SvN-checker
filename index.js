b2b045008f00d9b8ab75236b6bd374afddd46e85
Noah Reiter <n.reiter@alpin11.com>
2022-02-09 13:38:32 +0100

    finished

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 function check() {
+
+    //SvN examination (main)
     var identification = document.getElementById('identification').value;
     if (identification < 1000 || identification > 9999) {
-        alert('Persönliche Kennnummer zu klein/groß!');
+        alert('Identificationnumber to short/big!');
     } else {
         console.log("identification", identification);
     }
     var birthday = document.getElementById('birthday').value;
     if (birthday < 100000 || birthday > 999999) {
-        alert('Geburstag zu klein/groß!');
+        alert('Birthday to short/big!(TT.MM.YY)');
     } else {
         console.log("birthday", birthday);
     }
+
+    //Stringsadministration
     var input = String(identification) + String(birthday);
     var inputSplitted = [];
     inputSplitted = input.split("");
-    console.log(inputSplitted)
+
     var number1 = inputSplitted[0];
     var number2 = inputSplitted[1];
     var number3 = inputSplitted[2];
@@ -24,6 +28,7 @@ function check() {
     var number7 = inputSplitted[7];
     var number8 = inputSplitted[8];
     var number9 = inputSplitted[9];
+
     sumNumber1 = number1 * 3;
     sumNumber2 = number2 * 7;
     sumNumber3 = number3 * 9;
@@ -33,13 +38,11 @@ function check() {
     sumNumber7 = number7 * 2;
     sumNumber8 = number8 * 1;
     sumNumber9 = number9 * 6;
+
     summe = sumNumber1 + sumNumber2 + sumNumber3 + sumNumber4 + sumNumber5 + sumNumber6 + sumNumber7 + sumNumber8 + sumNumber9;
-
-
     result = summe % 11;
 
-    console.log(inputSplitted[3])
-    console.log("result", result)
+    //SvN examination
     if (String(result) == String(inputSplitted[3])) {
         document.getElementById('trueOrFalse').innerHTML = ('Die SvN ist gültig!');
     } else {
